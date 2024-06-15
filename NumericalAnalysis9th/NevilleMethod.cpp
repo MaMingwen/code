6ec5f0e7f165c7f5bec95189538f243d18096ab3
Omen16mmw <869994854@qq.com>
2024-06-15 11:40:45 +0800

    522

--- NumericalAnalysis9th/NevilleMethod.cpp
+++ NumericalAnalysis9th/NevilleMethod.cpp
@@ -12,6 +12,8 @@
 
 ## Fixed bugs
 1. After terminating a cin stream, the program can't excute second cin. The reason is that cin stream is under 'fail state' or read a false input or end-of-file.
+2. The Neville's formula was printed wrong, I have fixed it.
+3. Node method to fix bugs.
 */
 
 #include <iostream>
@@ -52,10 +54,42 @@ for(int i = 0;i<n;i++){
     f.push_back(fi);
 }
 
+
 //## Calculate.
 //### Create a matrix, and calculate the corresponding values.
+std::vector<std::vector<double>> table(n,std::vector<double>(n+1,0.0));
+//### The first column is x_i, i = 0,...,n
+for(int i = 0;i<n;i++){
+    table[i][0] = x[i];
+}
 
+//### The second column is f(x_i), i = 0,...n
+for(int i = 0;i<n;i++){
+    table[i][1] = f[i];
+}
 
+//### Use Neville Method to calculate the matrix
+//#### 这个公式市是错的table[i][j] = (a - table[i][0])/(table[i-1][0] - table[i][0])table[i-1][j-1] + (a - table[i-1][0])/(table[i][0] - talbe[i-1][0])table[i][j-1] i = 1,...,n;j = 2,...n.
+
+//dummy = (a - table[1][0])/(table[0][0] - table[1][0])*table[0][1] + (a - table[0][0])/(table[1][0] - table[0][0])*table[1][1];
+
+//talbe[1].push_back(dummy);
+for(int i = 1;i<n;i++){
+    for(int j = 2;j<=i+1;j++){
+        table[i][j] = (a - table[i][0])/(table[i-j+1][0] - table[i][0])*table[i-1][j-1] + (a - table[i-j+1][0])/(table[i][0] - table[i-j+1][0])*table[i][j-1];
+    }
+}
 //## Out the table.
+for(int i = 0;i<n;i++){
+std::cout << "The " << i+1 << "st row: ";
+    for(int j = 0;j<=i+1;j++){
+    std::cout << " "<<table[i][j]<<" ";
+}
+std::cout<<std::endl;
+}
+
+std::cout<<"done!"<<std::endl;
+
+std::cout<<"Test";
 
 }
